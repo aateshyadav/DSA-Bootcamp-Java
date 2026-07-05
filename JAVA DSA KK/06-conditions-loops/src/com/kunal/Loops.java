fcf9bbabaac1d64302c12168d5f0b6258915c8b6
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-05 23:50:50 +0530

    Update Loops.java with do-while loop and input
    Refactor loop examples and add user input for iterations.

--- JAVA DSA KK/06-conditions-loops/src/com/kunal/Loops.java
+++ JAVA DSA KK/06-conditions-loops/src/com/kunal/Loops.java
@@ -4,11 +4,60 @@ import java.util.Scanner;
 
 public class Loops {
     static void main(String[] args) {
+        // for loops
         // Q: print numbers one to five
 
         //int i;
-        for(int i=1;i<=5;i+=1){
-            System.out.println(i);
+//        for(int i=1;i<=5;i+=1){
+//            System.out.println(i);
+//        }
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+//
+//        for(int i=1; i<=n; i++){
+//            System.out.println("hello world");
+//        }
+
+        // while loops
+        /*
+        Syntax:
+        while(condition){
+        // body
         }
+         */
+
+//        int i=1;
+//        while(i<=n){
+//            System.out.println(i);
+//            i += 1;
+//        }
+
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=n);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
