84418aef2012af94426da6e2b696f7fcf7af0075
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-08 00:13:29 +0530

    Add basic calculator functionality
    Implemented a simple calculator that performs basic arithmetic operations based on user input.

--- JAVA DSA KK/06-conditions-loops/src/com/kunal/Calculator.java
+++ JAVA DSA KK/06-conditions-loops/src/com/kunal/Calculator.java
@@ -1,4 +1,45 @@
 package com.kunal;
 
+import java.util.Scanner;
+
 public class Calculator {
+    static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // Take input from user till user does not press X or x
+        int ans = 0;
+        while (true) {
+            // take the operator as input
+            System.out.println("ENTER THE OPERATOR: ");
+            char op = in.next().trim().charAt(0);
+            if (op == '+' || op == '-' ||  op == '*' || op == '/' || op == '%') {
+                // input two numbers
+                System.out.println("ENTER TWO NUMBERS: ");
+                int num1=in.nextInt();
+                int num2=in.nextInt();
+                
+                if(op == '+'){
+                    ans = num1 + num2;
+                }
+                if(op == '-'){
+                    ans = num1 - num2;
+                }
+                if(op == '*'){
+                    ans = num1 * num2;
+                }
+                if(op == '/'){
+                    if(num2!=0){
+                        num2 = num2 / num1;
+                    }
+                }
+                if (op == '%') {
+                    ans = num1 % num2;
+                }
+            } else if(op == 'x' || op == 'X') {
+                break;
+            } else {
+                System.out.println("Invalid operation!");
+            }
+            System.out.println(ans);
+        }
+    }
 }
