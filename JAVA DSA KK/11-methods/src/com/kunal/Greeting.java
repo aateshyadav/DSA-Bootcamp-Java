4ea9b27c4b8a34410dd46118558245573f3492ee
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-09 02:33:20 +0530

    Add files via upload

---
+++ JAVA DSA KK/11-methods/src/com/kunal/Greeting.java
@@ -0,0 +1,20 @@
+package com.kunal;
+
+import java.util.Scanner;
+
+public class Greeting {
+    static void main(String[] args) {
+//        greeting();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the name: ");
+        String naam = in.next();
+        String personalised =  greeting(naam);
+        System.out.println(personalised);
+
+    }
+
+    static String greeting(String name){
+        String message = "Hello " + name;
+        return message;
+    }
+}
---
+++ JAVA DSA KK/11-methods/src/com/kunal/StringExample.java
@@ -0,0 +1,12 @@
+package com.kunal;
+
+public class StringExample {
+    static void main(String[] args) {
+        String str = greet();
+        System.out.println(str);
+    }
+    static String greet(){
+        String greeting="how are you";
+        return greeting;
+    }
+}
