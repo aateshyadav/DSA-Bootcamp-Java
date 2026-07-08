fbc9d86dfa2ec0613dda4eeb4cffc228b49607d4
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-09 02:32:01 +0530

    Implement sum3 method and update main method
    Added sum3 method to calculate the sum of three integers and updated main method to call it. Retained sum2 and sum methods for future use.

--- JAVA DSA KK/11-methods/src/com/kunal/Sum.java
+++ JAVA DSA KK/11-methods/src/com/kunal/Sum.java
@@ -1,10 +1,47 @@
 package com.kunal;
 
+import java.util.Scanner;
+
 public class Sum {
     static void main(String[] args) {
-        sum();
+//        int ans = sum2();
+//        System.out.println(ans);
+        int ans = sum3(10,20,30);
+        System.out.println(ans);
     }
+
+    static int sum3(int a, int b, int c) {
+        int sum = a + b + c;
+        return sum;
+    };
+
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        int num1, num2, sum;
+
+        System.out.print("Enter number 1: ");
+        num1 = in.nextInt();
+
+        System.out.print("Enter number 2: ");
+        num2 = in.nextInt();
+
+        sum = num1 + num2;
+        return sum;
+        // System.out.println("hello"); -> Nothing run after this
+    }
+
+
     static void sum(){
         Scanner in = new Scanner(System.in);
+        int num1, num2, sum;
+
+        System.out.print("Enter number 1: ");
+        num1 = in.nextInt();
+
+        System.out.print("Enter number 2: ");
+        num2 = in.nextInt();
+
+        sum = num1 + num2;
+        System.out.println("The sum is " + sum);
     }
 }
