f4fddf869acf81087c6881d52bb1f29381b565cc
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-09 22:11:50 +0530

    Add files via upload

---
+++ JAVA DSA KK/11-methods/src/com/kunal/Swap.java
@@ -0,0 +1,15 @@
+package com.kunal;
+
+public class Swap {
+    static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        // Swap numbers code
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a : "+a + "\n" +"b : "+b);
+    }
+}
