f384576262e01af82fc9bb71bdbebb1b6e801401
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-10 23:32:20 +0530

    Add files via upload

---
+++ JAVA DSA KK/11-methods/src/com/kunal/PassingExample.java
@@ -0,0 +1,12 @@
+package com.kunal;
+
+public class PassingExample {
+    static void main(String[] args) {
+        String name = "Kunal Kushwaha";
+        greet(name);
+    }
+
+    static void greet(String naam){
+        System.out.println(naam);
+    }
+}
