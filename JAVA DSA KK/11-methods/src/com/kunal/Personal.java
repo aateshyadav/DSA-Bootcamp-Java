a36feb97a08d1dad044f661c17f2d8c0e9920625
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-15 02:38:58 +0530

    Add files via upload

---
+++ JAVA DSA KK/11-methods/src/com/kunal/Personal.java
@@ -0,0 +1,13 @@
+package com.kunal;
+
+public class Personal {
+    static void main(String[] args) {
+        int a = 1;
+        System.out.println(a);
+        {
+            a=2;
+            System.out.println(a);
+        }
+        System.out.println(a);
+    }
+}
