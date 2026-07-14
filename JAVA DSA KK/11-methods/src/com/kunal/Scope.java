a7a1886d095772eaf53f39cb9b252454ad633c16
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-15 02:37:55 +0530

    Refactor variable declarations in Scope class
    Updated variable scope and initialization in main method.

--- JAVA DSA KK/11-methods/src/com/kunal/Scope.java
+++ JAVA DSA KK/11-methods/src/com/kunal/Scope.java
@@ -2,9 +2,16 @@ package com.kunal;
 
 public class Scope {
     static void main(String[] args) {
-        int a = 10;
+       // int a = 10;    // intialising
         int b = 20;
-        System.out.println(num);
+        String name = "kunal";
+        {
+            int a=99;     // updating
+            name = "rahul";
+        }
+        int a =100;
+        System.out.println(a);
+        System.out.println(name);
     }
     static void random(int marks){
         int num = 67;
