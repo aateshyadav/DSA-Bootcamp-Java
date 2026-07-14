7e1c70a305ddae361b11a01e5c8bf949bbd39293
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-14 23:46:39 +0530

    Add files via upload

---
+++ JAVA DSA KK/11-methods/src/com/kunal/Scope.java
@@ -0,0 +1,14 @@
+package com.kunal;
+
+public class Scope {
+    static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(num);
+    }
+    static void random(int marks){
+        int num = 67;
+        System.out.println(num);
+        System.out.println(marks);
+    }
+}
