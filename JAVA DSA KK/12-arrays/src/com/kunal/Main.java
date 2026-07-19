06d67d00d6e4ec8601473337d92ea6c9aa5202e5
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-19 21:45:43 +0530

    Implement array declaration and initialization
    Added array declarations and initializations for integers and strings.

--- JAVA DSA KK/12-arrays/src/com/kunal/Main.java
+++ JAVA DSA KK/12-arrays/src/com/kunal/Main.java
@@ -9,6 +9,15 @@ public class Main {
         String name = "kunal kushwaha";
 
         // Q: store 5 roll numbers
-        
+//        int[] rnos = new int[5];
+//        int[] rnos2 = {23,12,45,32,15};
+
+        int[] ros; // declaration of an array. ros is getting defined in the stack
+        ros = new int[5]; // initlisation: actually here objects is being created in the heap memory
+        System.out.println(ros[1]);
+
+        String[] arr = new String[4];
+        System.out.println(arr[1]);
+
     }
 }
