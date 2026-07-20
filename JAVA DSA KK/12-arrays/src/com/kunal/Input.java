4d2d18b5c06b0b1aabd49e4c3c5348ab40378b7a
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-20 22:45:19 +0530

    Implement string array input and output
    Added functionality to read and print an array of strings.

--- JAVA DSA KK/12-arrays/src/com/kunal/Input.java
+++ JAVA DSA KK/12-arrays/src/com/kunal/Input.java
@@ -1,7 +1,29 @@
 package com.kunal;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Input {
     static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+//        int[] arr = new int[5];
+//        for(int i=0; i<arr.length;i++){
+//            arr[i] = in.nextInt();
+//        }
+//        for (int j : arr) {
+//            System.out.print(j + " ");
+//        }
+        //System.out.println(Arrays.toString(arr));
+
+        String[] str =  new String[4];
+        for (int i = 0; i < str.length; i++) {
+            str[i]= in.next();
+        }
+        System.out.println(Arrays.toString(str));
+
+
 
     }
+
 }
