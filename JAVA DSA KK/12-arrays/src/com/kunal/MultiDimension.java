4fd56120d4b2ac855d020a89054846314b345964
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-22 22:18:48 +0530

    Refactor MultiDimension to handle user input for 2D array
    Updated MultiDimension class to include user input for a 2D array and output the array using Arrays.toString().

--- JAVA DSA KK/12-arrays/src/com/kunal/MultiDimension.java
+++ JAVA DSA KK/12-arrays/src/com/kunal/MultiDimension.java
@@ -1,4 +1,6 @@
 package com.kunal;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class MultiDimension {
     public static void main(String[] args) {
@@ -7,11 +9,34 @@ public class MultiDimension {
         4  5  6
         7  8  9
          */
+        Scanner in = new Scanner(System.in);
         // int[][] arr = new int[3[3];
-        int[][] arr = {
-                {1,2},    // individual array can vary
-                {4,5,6,10},
-                {7,8,9}
-        }; 
+//        int[][] arr = {
+//                {1,2},    // individual array can vary
+//                {4,5,6,10},
+//                {7,8,9}
+//        };
+        int[][] arr=new int[3][3];
+        // input
+        for(int row=0;row<arr.length;row++){
+            // for each col in every row
+            for(int col=0;col<arr[row].length;col++){
+                arr[row][col]=in.nextInt();
+            }
+        }
+        
+        // output
+        for(int row=0;row<arr.length;row++){
+            System.out.println(Arrays.toString(arr[row]));
+        }
+
+        // output
+//        for(int row=0;row<arr.length;row++){
+//            // for each col in every row
+//            for(int col=0;col<arr[row].length;col++){
+//                System.out.print(arr[row][col] + " ");
+//            }
+//            System.out.println();
+//        }
     }
 }
