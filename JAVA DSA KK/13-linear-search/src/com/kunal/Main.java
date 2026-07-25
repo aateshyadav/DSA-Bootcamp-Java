b398c803ef43828567bf4f257c0d69eabd7015de
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-26 04:15:33 +0530

    Update linear search target and array values

--- JAVA DSA KK/13-linear-search/src/com/kunal/Main.java
+++ JAVA DSA KK/13-linear-search/src/com/kunal/Main.java
@@ -2,14 +2,15 @@ package com.kunal;
 
 public class Main {
     static void main(String[] args) {
-        int[] arr={1,2,3,4,5,-5,-4,-3,-2,-1,0};
-        int target=5;
-        int ans = linearSearch(arr,target);
+        int[] nums={1,2,3,4,5,-5,-4,-3,-2,-1,0};
+        int target= -2;
+        int ans = linearSearch(nums,target);
         System.out.println(ans);
     }
 
     // search in the array : return the index if item found
     // otherwise if item not found return -1
+
     static int linearSearch(int[] arr, int target){
         if(arr.length == 0){
             return -1;
@@ -28,4 +29,22 @@ public class Main {
         // hence the target not found
         return -1;
     }
+
+    // search the target and return the element
+//    static int linearSearch2(int[] arr, int target){
+//        if(arr.length == 0){        // arr.length is a variable , and it's without 
+//            return -1;
+//        }
+//
+//        // run a for loop
+//        for(int element : arr){
+//            if(element == target){
+//                return element;
+//            }
+//        }
+//
+//        // this line will execute if none of the return statements above have executed
+//        // hence the target not found
+//        return -1;
+//    }
 }
