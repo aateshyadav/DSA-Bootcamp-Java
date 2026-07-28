8182bad03d354d83869a7a39294db431bd50618d
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-07-29 00:08:30 +0530

    Add files via upload

---
+++ JAVA DSA KK/14-binary-search/src/com/kunal/BinarySearch.java
@@ -0,0 +1,17 @@
+package com.kunal;
+
+public class BinarySearch {
+    static void main(String[] args) {
+
+    }
+    // return the index
+    static int binarySearch(int[] arr,int target){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            // find the middle element
+           // int mid = (start+end)/2;   // might be possible that (start=end)/2 exceeds the range of integer in java
+            int mid = start + (end-start)/2;
+        }
+    }
+}
