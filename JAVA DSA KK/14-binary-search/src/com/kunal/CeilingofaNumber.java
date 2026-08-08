5b939156ffbd96c03fad14e3957d8a3df9edd6fb
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-08-08 23:10:07 +0530

    Refactor ceilingofanumber method for accurate return
    Updated the ceilingofanumber method to return the smallest number greater than or equal to the target. Adjusted comments for clarity.

--- JAVA DSA KK/14-binary-search/src/com/kunal/CeilingofaNumber.java
+++ JAVA DSA KK/14-binary-search/src/com/kunal/CeilingofaNumber.java
@@ -1,28 +1,34 @@
 package com.kunal;
 
-public class CeilingofaNumber {
+public class CeilingofaNumber {   // smallest number greater than or equal to target
     static void main(String[] args) {
-        int[] arr = {2,3,5,9,14,16,18};
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
         int target = 15;
         int ans = ceilingofanumber(arr, target);
         System.out.println(ans);
     }
-    static int ceilingofanumber(int[] arr,int target){
+    static int ceilingofanumber(int[] arr, int target) {
         int start = 0;
-        int end = arr.length-1;
-        while(start<=end){
-            // find the middle element
-            // int mid = (start+end)/2;   // might be possible that (start=end)/2 exceeds the range of integer in java
-            int mid = start + (end-start)/2;
-            if(target<arr[mid]){
-                end = mid-1;
-            } else if(target>arr[mid]){
-                start = mid+1;
-            } else{
-                // ans found
-                return mid;
+        int end = arr.length - 1;
+        while (start <= end) {
+
+            // Find the middle element
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // target == arr[mid]
+                return arr[mid];
             }
         }
-        return -1;    // when element does not exist
+        // start points to the smallest element
+        // greater than the target
+//        if (start < arr.length) {
+//            return arr[start];
+//        }
+        // Ceiling does not exist
+        return arr[start];
     }
 }
