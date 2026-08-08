dd106ee935e7cf447d2ee6368ea8a1f126f201aa
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-08-09 01:30:54 +0530

    Refactor method to find smallest letter greater than target
    Refactored ceilingofanumber method to smallestnumber and updated logic to handle character arrays.

--- JAVA DSA KK/14-binary-search/src/com/kunal/SmallestLetter.java
+++ JAVA DSA KK/14-binary-search/src/com/kunal/SmallestLetter.java
@@ -4,31 +4,22 @@ public class SmallestLetter {
     static void main(String[] args) {
         char[] arr = {'c','f','j'};
         char target = 'a';
-        char ans = ceilingofanumber(arr, target);
+        char ans = smallestnumber(arr,target);
         System.out.println(ans);
     }
-    static int ceilingofanumber(int[] arr, int target) {
+    static char smallestnumber(char[] arr,char target) {
         int start = 0;
-        int end = arr.length - 1;
-        while (start <= end) {
+        int end = arr.length-1;
 
-            // Find the middle element
-            int mid = start + (end - start) / 2;
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
+        while(start<=end) {
+            int mid = start+(end-start)/2;
+
+            if(target<arr[mid]) {
+                end = mid-1;
             } else {
-                // target == arr[mid]
-                return arr[mid];
+                start = mid+1;
             }
         }
-        // start points to the smallest element
-        // greater than the target
-//        if (start < arr.length) {
-//            return arr[start];
-//        }
-        // Ceiling does not exist
-        return arr[start];
+        return arr[start%arr.length];
     }
 }
