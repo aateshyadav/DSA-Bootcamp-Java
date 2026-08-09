6189b375c55e03fa0d0c2570eb67b9f037c8c79c
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-08-10 01:32:36 +0530

    Add files via upload

---
+++ JAVA DSA KK/14-binary-search/src/com/kunal/FirstAndLastPosition.java
@@ -0,0 +1,45 @@
+package com.kunal;
+
+public class FirstAndLastPosition {
+    public static void main(String[] args) {
+
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+
+        // check for first occurrence if target first
+
+        int start = search( nums, target, true);
+        if (ans[0] != -1) {
+            ans[1] = search(nums, target, false);
+        }
+        return ans;
+//        ans[0]=start;
+//        ans[1]=end;
+    }
+    // this function just returns the index value of target
+    int search(int[] nums, int target,boolean findStartIndex){
+        int start = 0;
+        int ans=-1;
+        int end = nums.length-1;
+        while(start<=end){
+            // find the middle element
+            // int mid = (start+end)/2;   // might be possible that (start=end)/2 exceeds the range of integer in java
+            int mid = start + (end-start)/2;
+            if(target<nums[mid]){
+                end = mid-1;
+            } else if(target>nums[mid]){
+                start = mid+1;
+            } else{
+                // potential ans found
+                ans = mid;
+                if(findStartIndex){
+                    end=mid-1;
+                } else{
+                    start = mid+1;
+                }
+            }
+        }
+        return ans;
+    }
+}
