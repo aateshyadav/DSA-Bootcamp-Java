cff00840b0b163c56e216bb72494f97d9909d056
Aatesh Yadav <114991525+aateshyadav@users.noreply.github.com>
2026-08-11 23:07:05 +0530

    Implement search in infinite sorted array
    Added implementation for searching in an infinite array.

--- JAVA DSA KK/14-binary-search/src/com/kunal/InfiniteArray.java
+++ JAVA DSA KK/14-binary-search/src/com/kunal/InfiniteArray.java
@@ -2,21 +2,25 @@ package com.kunal;
 
 public class InfiniteArray {
     public static void main(String[] args) {
+        int[] arr={3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170};
+        int target=10;
+        System.out.println(ans(arr,target));
     }
     static int ans(int[] arr,int target){
         // first find the range
         // find start with a box of size 2
         int start=0;
-        int end=1;
+        int end=1;   // because we don't know the array length
 
         // condition for the target to lie in the range
         while(target>arr[end]){
-            int newStart=end+1;
+            int temp=end+1; // this is my new start
             // double the box value
-            // end = previous end + sizeof box*2
+            // end = previous end + size of box*2
             end = end + (end - start + 1) * 2;
+            start = temp;
         }
-
+        return binarySearch(arr, target, start, end);
     }
     static int binarySearch(int[] arr,int target,int start,int end){
         while(start<=end){
